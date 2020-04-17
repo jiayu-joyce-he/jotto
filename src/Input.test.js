431ce036111f2168b4120e3c7ffe6e4da967dd58
jiayu-joyce-he <jiayuhe@alumni.stanford.edu>
2020-04-17 11:30:05 -0700

    complete input test for success=true

--- src/Input.test.js
+++ src/Input.test.js
@@ -47,11 +47,25 @@ describe('render', () => {
     })
 
     describe('words has been guessed', () => {
-        test('does not render component without error', () => {})
+        let wrapper
+        beforeEach(() => {
+            const initialState = { success: true }
+            wrapper = setup(initialState)
+        })
+        test('render component without error', () => {
+            const component = findByTestAttr(wrapper, 'component-input')
+            expect(component.length).toBe(1)
+        })
 
-        test('does not render input box', () => {})
+        test('does not render input box', () => {
+            const inputBox = findByTestAttr(wrapper, 'input-box')
+            expect(inputBox.length).toBe(0)
+        })
 
-        test('does not render submit button', () => {})
+        test('does not render submit button', () => {
+            const submitButton = findByTestAttr(wrapper, 'submit-button')
+            expect(submitButton.length).toBe(0)
+        })
     })
 })
 
